6a46cdada7108d934455dc6699a20cb298f55e1f
OscarLVz <emmanuellv@hotmail.com>
2023-08-19 11:18:10 -0600

    Preloader ok

--- src/app/components/commons/pre-loader/pre-loader.component.ts
+++ src/app/components/commons/pre-loader/pre-loader.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-pre-loader',
+  selector: 'pre-loader-component',
   templateUrl: './pre-loader.component.html',
   styleUrls: ['./pre-loader.component.scss']
 })
