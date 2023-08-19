d449e20added3db2e95013d848ba7e183bd29aee
OscarLVz <emmanuellv@hotmail.com>
2023-08-19 11:08:59 -0600

    Footer component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/commons/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/commons/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
