1214aa9121f3b5d4f5be9bd7415a2a2a2a1d4fcb
OscarLVz <emmanuellv@hotmail.com>
2023-09-20 22:42:52 -0600

    environments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,7 @@ export class AppComponent implements OnInit{
   title = 'papeleria-gratis';
 
   ngOnInit(): void {
-    console.log(":D");    
+    console.log(":D production: "+environment.production);    
   }
   
 }
