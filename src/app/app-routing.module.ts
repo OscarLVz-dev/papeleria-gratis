970100705ba62897cf096fa5b35777f7d2c6be5c
OscarLVz <emmanuellv@hotmail.com>
2023-10-11 23:18:17 -0600

    Categories page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { HomeCategoriesComponent } from './components/pages/home/home-categories/home-categories.component';
 
 const routes: Routes = [
+  { path: 'categorias', component: HomeCategoriesComponent },
   { path: '', component: HomeComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
