d23e1190a526f9f1316c8c5cecd8a4d0785bbbd0
OscarLVz <emmanuellv@hotmail.com>
2023-11-07 22:35:12 -0600

    Component base ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { HomeCategoriesComponent } from './components/pages/home/home-categories/home-categories.component';
+import { CategoryComponent } from './components/pages/category/category.component';
 
 const routes: Routes = [
   { path: 'categorias', component: HomeCategoriesComponent, pathMatch: 'prefix' },
+  { path: 'categoria', component: CategoryComponent, pathMatch: 'prefix' },
   { path: '', component: HomeComponent },
 ];
 
